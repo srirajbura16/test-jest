c54a9e4ad66f7032f45d68e7dfa85bc573c987ce
Sriraj Bura <sriraj.bura16@gmail.com>
2021-06-23 20:49:27 -0400

    implemented calculator object

---
+++ src/calculator.js
@@ -0,0 +1,16 @@
+const calculator = {
+  add: (a, b) => {
+    a + b;
+  },
+  subtract: (a, b) => {
+    a - b;
+  },
+  multiply: (a, b) => {
+    a * b;
+  },
+  divide: (a, b) => {
+    a / b;
+  },
+};
+
+module.exports = calculator;
