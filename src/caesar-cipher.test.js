32241ef2303fa6a569c0c31ad07693d995570475
Sriraj Bura <sriraj.bura16@gmail.com>
2021-07-14 19:50:49 -0400

    updated error message

--- src/caesar-cipher.test.js
+++ src/caesar-cipher.test.js
@@ -1,6 +1,6 @@
 const caesarCipher = require('./caesar-cipher');
 
-test('shifts the letters to specified in alaphbet.', () => {
+test('shifts the letters to specified value in alaphbet.', () => {
   expect(caesarCipher('hello', 5)).toBe('mjqqt');
   expect(caesarCipher('BlackWater', 5)).toBe('GqfhpBfyjw');
   expect(caesarCipher('code', 5)).toBe('htij');
--- src/calculator.test.js
+++ src/calculator.test.js
@@ -1,24 +1,24 @@
 const calculator = require('./calculator');
 
-test('add two number together', () => {
+test('add two numbers together', () => {
   expect(calculator.add(1, 1)).toBe(2);
   expect(calculator.add(55, 31)).toBe(86);
   expect(calculator.add(232, 451)).toBe(683);
 });
 
-test('add two number together', () => {
+test('subtract two numbers together', () => {
   expect(calculator.subtract(1, 1)).toBe(0);
   expect(calculator.subtract(55, 31)).toBe(24);
   expect(calculator.subtract(90, 22)).toBe(68);
 });
 
-test('add two number together', () => {
+test('multiply two numbers together', () => {
   expect(calculator.multiply(6, 2)).toBe(12);
   expect(calculator.multiply(10, 5)).toBe(50);
   expect(calculator.multiply(90, 3)).toBe(270);
 });
 
-test('add two number together', () => {
+test('divide two numbers together', () => {
   expect(calculator.divide(6, 2)).toBe(3);
   expect(calculator.divide(10, 5)).toBe(2);
   expect(calculator.divide(90, 3)).toBe(30);
